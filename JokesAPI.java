aecde2595ae08aec918f7cc69e01be1f70954cdd
riya <riya2@illinois.edu>
2018-12-12 03:10:52 -0600

    1

---
+++ JokesAPI.java
@@ -0,0 +1,15 @@
+package com.example.riya.calculatorapp;
+
+public class JokesAPI {
+
+
+    public static String getJoke() {
+        return "hahahahha";
+    }
+
+    public static String getJokeText() {
+        return "hahahahha";
+    }
+
+
+}
